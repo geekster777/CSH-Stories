5ad4acf183fdba066d5186a503b50458d8a42fe8
Nick Hilton <hilton@csh.rit.edu>
2014-11-27 01:18:06 +0000

    Updated the application to use SQLAlchemy.

--- stories.py
+++ stories.py
@@ -1,9 +1,22 @@
 import os
-from sqlite3 import dbapi2 as sqlite3
 from settings import db_config
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.update(db_config)
+db = SQLAlchemy(app)
 
-app.configure.update(db_config)
+class Story(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True)
+    author = db.Column(db.String(100))
+    title = db.Column(db.String(100), unique=True)
+    body = db.Column(db.Text)
+    pub_date = db.Column(db.DateTime)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(100))
+    body = db.Column(db.Text)
+
 
