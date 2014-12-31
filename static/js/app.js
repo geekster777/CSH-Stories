63d83b97b3b9c475006c147c667ae652c4e28033
Nick Hilton <hilton@csh.rit.edu>
2014-12-31 01:42:11 +0000

    The nav bar now becomes solid when the user scrolls down the page.

--- static/js/app.js
+++ static/js/app.js
@@ -1,11 +1,28 @@
 $().ready(function() {
-  var editor = new Quill('#editor', {
-    modules: {
-      'toolbar': { container: '#formatting-container' },
-      'link-tooltip': true,
-      'image-tooltip': true
-    },
-    styles: false,
-    theme: 'snow'
+
+  // fade in the top bar when the user scrolls down
+  $(document).scroll(function() {
+    var topBar = $('.top-bar');
+    if($(document).scrollTop() > 0) {
+      if(!topBar.hasClass('scrolled')) {
+        topBar.addClass('scrolled');
+      }
+    }
+    else {
+      topBar.removeClass('scrolled');
+    }
   });
+
+  // create an editor object when the user is on an add story page
+  if($('#editor').length) {
+    var editor = new Quill('#editor', {
+      modules: {
+        'toolbar': { container: '#formatting-container' },
+        'link-tooltip': true,
+        'image-tooltip': true
+      },
+      styles: false,
+      theme: 'snow'
+    });
+  }
 });
